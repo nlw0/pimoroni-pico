1566d8ae260c7bad2f25d4fb8033e4eeb8b9c456
Nicolau Leal Werneck <nicolau@deltaxlab.com>
2023-12-14 21:31:41 +0100

    original demo.cpp

--- libraries/pico_unicorn/pico_unicorn.cpp
+++ libraries/pico_unicorn/pico_unicorn.cpp
@@ -1,5 +1,5 @@
-#include "pico_unicorn.hpp"
+// #include "pico_unicorn.hpp"
 
-template class pimoroni::PicoUnicorn<14,1,0, uint16_t, pimoroni::GAMMA_14BIT>;
-template class pimoroni::PicoUnicorn<12,1,4, uint16_t, pimoroni::GAMMA_12BIT>;
-template class pimoroni::PicoUnicorn<12,6,4, uint16_t, pimoroni::GAMMA_12BIT>;
+// template class pimoroni::PicoUnicorn<14,1,0, uint16_t, pimoroni::GAMMA_14BIT>;
+// template class pimoroni::PicoUnicorn<12,1,4, uint16_t, pimoroni::GAMMA_12BIT>;
+// template class pimoroni::PicoUnicorn<12,6,4, uint16_t, pimoroni::GAMMA_12BIT>;
